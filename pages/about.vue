8e808c08d26c60168f57483ca7e9f241d4a20749
Nicholas Chug <nicholas.chug@gmail.com>
2023-02-10 11:49:48 -0500

    introduced NuxtLink router parameters to project

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/products">Products</NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus qui maxime illo temporibus ab dolore incidunt quasi unde voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic necessitatibus qui maxime illo temporibus ab dolore incidunt quasi unde voluptatum.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+      h2 {
+          margin-bottom: 20px;
+          font-size: 36px;
+      }
+      p {
+          margin: 20px 0;
+      }
+  </style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem amet a totam, tempore dolorem perferendis ipsum quis iusto ratione reiciendis!</p>
+  </div>
+</template>
+
+<script setup>
+
+    const { id } = useRoute().params
+
+</script>
+
+<style scoped>
+
+</style>
